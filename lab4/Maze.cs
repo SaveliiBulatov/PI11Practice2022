f3426012ddbd7931d3c051875c556e23b2ecad25
BulatovSavulii <118110774+SaveliiBulatov@users.noreply.github.com>
2022-12-22 17:54:59 +0400

    Sava 4

---
+++ lab4/Maze.cs
@@ -0,0 +1,134 @@
+class Maze
+{
+
+    //данные
+
+    public bool key1 = false;
+    public int coins = 0;
+    public bool door = false;
+    public bool score = false;
+    int playerx = 2;
+    int playery = 1;
+    public int[,] maze = new int[,]
+    {
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 
+                { 1, 3, 0, 1, 5, 0, 1, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1 }, 
+                { 1, 0, 1, 5, 0, 0, 1, 0, 0, 0, 1, 0, 1 },
+                { 1, 0, 1, 1, 1, 0, 1, 0, 0, 5, 1, 0, 1 }, 
+                { 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1 },
+                { 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1 }, 
+                { 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1 },
+                { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, 
+                { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+                { 1, 0, 1, 1, 5, 1, 0, 0, 0, 1, 0, 0, 1 }, 
+                { 1, 0, 0, 0, 0, 1, 5, 1, 0, 0, 0, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+    };
+    ConsoleColor ink;
+    ConsoleColor paper;
+
+    public Maze(ConsoleColor i, ConsoleColor p)
+    {
+        ink = i;
+        paper = p;
+    }
+    //методы
+    public void Move(int dx, int dy)
+    {
+        int nx = playerx + dx;
+        int ny = playery + dy;
+
+
+        if (maze[ny, nx] > maze.Length)
+        {
+            Console.WriteLine("Выход за пределы массива");
+
+        }
+
+        if (maze[ny, nx] == 0 || maze[ny, nx] == 4 || maze[ny, nx] == 5)
+        {
+            playerx = nx;
+            playery = ny;
+        }
+        if (maze[ny, nx] == 2 && coins == 5)
+        {
+            Print(3, 24, "Вы купили ключ");
+            key1 = true;
+            playerx = 1;
+            playery = 1;
+            maze[ny, nx] = 0;
+            maze = new int[,]
+            {
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 }, 
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 }, 
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 }, 
+                { 1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+            };
+
+        }
+
+
+
+
+        if (maze[ny, nx] == 4)
+        {
+            door = true;
+        }
+        if (maze[ny, nx] == 5)
+        {
+            maze[ny, nx] = 0;
+            coins++;
+            score = true;
+        }
+    }
+
+
+
+    public void Print(int shiftx, int shifty)
+    {
+        for (int y = 0; y < 13; y++)
+            for (int x = 0; x < 13; x++)
+            {
+                double dist = Math.Sqrt((playerx - x) * (playerx - x) + (playery - y) * (playery - y));
+                if (dist > 5)
+                {
+                    Print(shiftx + x, shifty + y, " ", ConsoleColor.White, ConsoleColor.DarkBlue);
+                }
+                else
+                {
+                    if (maze[y, x] == 0) Print(shiftx + x, shifty + y, ".");
+                    else if (maze[y, x] == 1) Print(shiftx + x, shifty + y, "#", ink, paper);
+                    else if (maze[y, x] == 2) Print(shiftx + x, shifty + y, "&");
+                    else if (maze[y, x] == 3) Print(shiftx + x, shifty + y, "|");
+                    else if (maze[y, x] == 4) Print(shiftx + x, shifty + y, "%");
+                    else if (maze[y, x] == 5) Print(shiftx + x, shifty + y, "$");
+                }
+            }
+
+        Print(shiftx + playerx, shifty + playery, "@");
+    }
+
+    public void Print(int x, int y, string s, ConsoleColor ink = ConsoleColor.Blue, ConsoleColor paper = ConsoleColor.Black)
+    {
+        Console.ForegroundColor = ink;
+        Console.BackgroundColor = paper;
+        Console.CursorLeft = x;
+        Console.CursorTop = y;
+        Console.Write(s);
+
+
+
+    }
+
+
+}
---
+++ lab4/Program.cs
@@ -0,0 +1,71 @@
+﻿Console.Clear();
+
+Maze m = new Maze(ConsoleColor.Red, ConsoleColor.Yellow);
+
+while (true)
+{
+    m.Print(5, 19, "Вы оказались в лабиринте.Вам нужно собрать монетки,купить на них ключ и открыть им дверь.Это условия для победы");
+
+
+    m.Print(3, 3);
+    if (m.door)
+    {
+        Console.CursorVisible = false;
+        Console.Clear();
+        m.Print(5, 25, "Вы выбрались");
+        
+    }
+    if (m.score)
+    {
+        m.score = false;
+        m.Print(5, 20, $"вы собрали {m.coins} монет");
+        //
+        if (m.coins == 5)
+        {
+
+            m.Print(5, 21, "Вы можете купить ключ");
+ 
+            m.maze = new int[,]
+    {
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 }, 
+                { 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1 }, 
+                { 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+                { 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 }, 
+                { 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1 }, 
+                { 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1 },
+                { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 }, 
+                { 1, 3, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+    };
+
+            if (m.key1 == true)
+            {
+                m.Print(5, 22, "Вы подобрали ключ");
+            }
+
+
+        }
+
+
+
+
+    }
+
+
+    ConsoleKeyInfo ki = Console.ReadKey(true);
+    if (ki.Key == ConsoleKey.Escape)
+    {
+
+        Console.Clear();
+        break;
+    }
+    //перемещение
+    if (ki.Key == ConsoleKey.LeftArrow) m.Move(-1, 0);
+    if (ki.Key == ConsoleKey.RightArrow) m.Move(1, 0);
+    if (ki.Key == ConsoleKey.UpArrow) m.Move(0, -1);
+    if (ki.Key == ConsoleKey.DownArrow) m.Move(0, 1);
+}
